c24c3daa607bd0cd6f5dbcbf486194074f7abd20
Ethan Eill <97324170+Ethan-Eill@users.noreply.github.com>
2023-12-02 16:32:41 -0500

    [WIP]<AOC>: Day 1 - Trebuchet
    [WIP]
    Part 1 of the puzzle completed successfully
    
    <Changes>
    	modified:   README.md
    	modified:   main
    	modified:   main.cpp

--- Day1 - Trebuchet/main.cpp
+++ Day1 - Trebuchet/main.cpp
@@ -19,21 +19,46 @@
 //---------------------------------------------------------------------------
 /// @brief get the given lines calibration value.
 /// @param  string    current_line        current line in check from the input file
-/// @return int       calibration_value   the first number in the string * 10, and the last number in the string added together
+/// @return uint8_t   calibration_value   the first number in the string * 10, and the last number in the string added together
 //---------------------------------------------------------------------------
 int get_current_calibration_value(std::string current_line){
-    int calibration_value = 0;
-    int index = 0;
+    uint8_t calibration_value = 0;
+    uint16_t index = 0;
     bool is_digit_found = false;
 
+    //std::cout << current_line << std::endl;
+
+    // loop from the start of string until the first digit is found
     while(!is_digit_found){
-        if(isDigit(current_line[index])){
-            
+        // if that digit is found, add that multiplied by 10
+        // to the calibration value
+        // otherwise, increment to next char in string
+        if(isdigit(current_line[index])){
+            calibration_value += (current_line[index] - '0') * 10;
+            is_digit_found = true;
         }else{
             index++;
         }
-    }
+    }   // while digit is not found end
 
+    // reset index to end of the string
+    index = current_line.size() - 1;
+    is_digit_found = false;
+
+    // loop from the end of string until a digit is found
+    while(!is_digit_found){
+        // if that digit is found, add that
+        // to the calibration value
+        // otherwise, increment to next char in string
+        if(isdigit(current_line[index])){
+            calibration_value += (current_line[index] - '0');
+            is_digit_found = true;
+        }else{
+            index--;
+        }
+    }   // while digit is not found end
+
+    //std::cout << calibration_value << std::endl;
     return calibration_value;
 }
 
@@ -53,12 +78,14 @@ int main(int argc, char** argv){
 
     // Loop through and get each line in input file
     while(std::getline(file_pointer, current_line)){
+        // get this lines current calibration value
+        current_lines_calibration_value = get_current_calibration_value(current_line);
 
-
-
+        // add that to the final calibration value
+        calibration_value += current_lines_calibration_value;
     }   // while loop through file end
     
-
+    std::cout << calibration_value << std::endl;
 
     return 0;
 }
